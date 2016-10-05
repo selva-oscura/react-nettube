e6a18878524167e25ed6d194324c7f3d1e312eeb
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-04 21:15:25 -0700

    added routing for switching between Home and Search views

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const ReactRouter = require('react-router');
+// const Router = ReactRouter.router;
+// const Route = ReactRouter.route;
+// const hashHistory = ReactRouter.hashHistory;
+/* destructuring to get the components in one line rather than the three above */
+const { Router, Route, hashHistory } = ReactRouter;
 
 const Home = require('./Home');
+const Search = require('./Search');
 
 const App = () => (
-  <div className = 'app-container'>
-  	<Home />
-  </div>
+  <Router history={hashHistory}>
+    <Route path='/' component={Home} />
+    <Route path='/search' component={Search} />
+
+  </Router>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
