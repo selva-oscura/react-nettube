f567bddda27c8759f2982586dfeda0c58a953b37
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-04 21:16:32 -0700

    created link from Home to Search

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 const React = require('react');
+const ReactRouter = require('react-router');
+const { Link } = ReactRouter;
 
 const Home = () => (
   <div className = 'app-container'>	
     <div className='home-info'>
       <h1 className='title'>Bash Videos</h1>
-      <button className='browse-all'>Browse All</button>
+      <Link to='/search' className='browse-all'>Browse All</Link>
     </div>
   </div>
 )
