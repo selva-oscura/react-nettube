9f52ee04f2a1f0b2553cd7e33a079db6eed74e8e
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-18 19:32:30 -0700

    changed text on link

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,10 @@ const ReactRouter = require('react-router');
 const { Link } = ReactRouter;
 
 const Home = () => (
-  <div className = 'app-container'>	
+  <div className = 'app-container home-image'>	
     <div className='home-info'>
       <h1 className='title'>Bash Videos</h1>
-      <Link to='/search' className='browse-all'>Browse All</Link>
+      <Link to='/search' className='browse-all'>Go to Search</Link>
     </div>
   </div>
 )
