97106244142ba86b0448082e3accbf7288f66fc7
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-18 21:04:00 -0700

    displayed 'show cards' for each show

--- src/Search.js
+++ src/Search.js
@@ -2,20 +2,28 @@ const React = require('react');
 const ReactRouter = require('react-router');
 const { Link } = ReactRouter;
 import data from '../public/data';
-console.log('data', data);
+// console.log('data', data);
 
-const Search = () => (
-  <div className='app-container search-image'>
-    <div className='home-info'>
-      <h1>Ye Olde Searche</h1>
-      <Link to='/' className='browse-all'>Back to Home</Link>
-      <pre>
-      	<code>
-      		{JSON.stringify(data, null, 4)}
-      	</code>
-      </pre>
-    </div>
-  </div>
-);
+const Search = () => {
+	return (
+	  <div className='app-container'>
+	  	<div className='container'>
+		  	<div className='shows'>
+		  		{data.shows.map((show) => ( 
+						<div className='show' key={show.imdbID}>
+							<img className='show-card-img' src={`./public/img/posters/${show.poster}`} />
+							<div className='show-card-text' key={show.imdbID}>
+								<h3 className='show-card-title'>{show.title}</h3>
+								<h4 className='show-card-year'>{show.year}</h4>
+								<p className='show-card-description'>{show.description}</p>
+							</div>
+						</div>
+
+		  		))}
+		  	</div>
+	  	</div>
+	  </div>
+	)
+};
 
 module.exports = Search;
