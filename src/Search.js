7d5059273f12e01779805bec3b5db8e75410d8a0
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-18 19:36:10 -0700

    imported data from /public/data.json and displayed on the page

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,19 @@
 const React = require('react');
 const ReactRouter = require('react-router');
 const { Link } = ReactRouter;
+import data from '../public/data';
+console.log('data', data);
 
 const Search = () => (
   <div className='app-container search-image'>
     <div className='home-info'>
       <h1>Ye Olde Searche</h1>
       <Link to='/' className='browse-all'>Back to Home</Link>
+      <pre>
+      	<code>
+      		{JSON.stringify(data, null, 4)}
+      	</code>
+      </pre>
     </div>
   </div>
 );
