c464bf5b76a0e70dd94b71c85fa11e1df68ea871
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-18 19:34:37 -0700

    added react-router and link from react-router, added link back to home, and changed background picture

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,14 @@
 const React = require('react');
+const ReactRouter = require('react-router');
+const { Link } = ReactRouter;
 
 const Search = () => (
-	<div className='app-container'>
-		<h1>Ye Olde Searche</h1>
-	</div>
+  <div className='app-container search-image'>
+    <div className='home-info'>
+      <h1>Ye Olde Searche</h1>
+      <Link to='/' className='browse-all'>Back to Home</Link>
+    </div>
+  </div>
 );
 
 module.exports = Search;
