65fc017338d6673ca59a6e37a829993315a6b7b6
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-19 22:38:31 -0700

    added state parameter and display default searchText

--- src/Search.js
+++ src/Search.js
@@ -5,10 +5,11 @@ import ShowCard from './ShowCard';
 import data from '../public/data';
 // console.log('data', data);
 
-const Search = () => {
+const Search = (state) => {
 	return (
 	  <div className='app-container'>
 	  	<div className='container'>
+				<h2>{state.searchText}</h2> 
 		  	<div className='shows'>
 		  		{data.shows.map((show) => ( 
 		  			<ShowCard key={show.imdbID} show={show} />
