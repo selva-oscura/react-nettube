1676f786bffc27928fa152189c754c57a1acd3d4
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-19 22:37:34 -0700

    added props, state, default text for state.searchText and passed state searchText to Search Component

--- src/app.js
+++ src/app.js
@@ -10,12 +10,22 @@ const { Router, Route, hashHistory } = ReactRouter;
 const Home = require('./Home');
 const Search = require('./Search');
 
-const App = () => (
-  <Router history={hashHistory}>
-    <Route path='/' component={Home} />
-    <Route path='/search' component={Search} />
-
-  </Router>
-)
+class App extends React.Component{	
+	constructor(props){
+		super(props);
+		this.state={
+			searchText: "default text"
+		}
+		console.log("this.state", this.state)
+	}
+	render(){
+		return (
+		  <Router history={hashHistory}>
+		    <Route path='/' component={() => <Home />} />
+		    <Route path='/search' component={() => <Search searchText={this.state.searchText} />} />
+		  </Router>
+		)
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById('app'))
