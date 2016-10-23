2229215ee37a376ef83dc19bdcf2715ecf1d2cfb
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-22 18:17:57 -0700

    title sort of returned shows

--- src/Search.js
+++ src/Search.js
@@ -19,6 +19,15 @@ const Search = ({searchText, updateSearchText}) => {
 	}else{
 		shows = data.shows;
 	}
+	shows = shows.sort(function(a,b){ 
+		if(a.title < b.title){
+			return -1;
+		}
+		if(a.title>b.title){
+			return 1;
+		}
+		return 0;
+	});
 	return (
 	  <div className='app-container'>
 	  	<div className='container'>
