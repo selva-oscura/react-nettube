c628b93a0de7ea29a832396051c5d662766924f0
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-25 21:02:12 -0700

    removed state from App and passing of state to Search component

--- src/App.js
+++ src/App.js
@@ -5,31 +5,18 @@ import ReactDOM from 'react-dom';
 // const Route = ReactRouter.route;
 // const hashHistory = ReactRouter.hashHistory;
 /* destructuring to get the components in one line rather than the three above */
-import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Layout from './Layout';
 import Home from './Home';
 import Search from './Search';
 
-class App extends Component{	
-	constructor(props){
-		super(props);
-		this.state= { searchText: "" };
-	}
-	updateSearchText(e){
-		this.setState({ searchText: e.target.value });
-	}
-
-	render(){
-		return (
-			<Router history={hashHistory}>
-				<Route path="/" component={Layout}>
-				  <IndexRoute component={Home} />
-				  <Route path='/search' component={() => <Search searchText={this.state.searchText} updateSearchText={this.updateSearchText.bind(this)} />} 
-				  />
-				</Route>
-			</Router>
-		)
-	}
-}
+const App = () => (
+	<Router history={hashHistory}>
+		<Route path="/" component={Layout}>
+		  <IndexRoute component={Home} />
+		  <Route path='/search' component={Search} />
+		</Route>
+	</Router>
+)
 
 ReactDOM.render(<App />, document.getElementById('app'))
