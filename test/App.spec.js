c3acd5fcc703e916899ccbdfdc422fcc38db2792
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-06 20:54:43 -0800

    tested that DOM renders all shows in data.json

--- test/App.spec.js
+++ test/App.spec.js
@@ -3,9 +3,9 @@
 import { expect } from 'chai';
 import React from 'react';
 import Search from '../src/Search';
-// import ShowCard from '../src/ShowCard';
+import ShowCard from '../src/ShowCard';
 import { shallow, mount } from 'enzyme';
-import {shows} from '../public/data';
+import { shows } from '../public/data';
 
 // describe('<Search />', () => {
 // 	it('should pass ', () => {
@@ -26,7 +26,9 @@ describe('<Search />', () => {
 		console.log(wrapper.debug());
 		expect(wrapper.contains(<h2 className='brand'>Bash Videos</h2>)).to.be.true;
 	});
+	it('should render as many shows as there are data for', () => {
+		const wrapper = shallow(<Search />);
+		expect(wrapper.find(ShowCard).length).to.equal(shows.length);
+	})
 }); 
-
-
 
