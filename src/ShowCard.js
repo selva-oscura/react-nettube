0efb715c3994ce45f4250dc20721fcb3f3cb8385
Carol St.Louis <1.selva.oscura@gmail.com>
2016-12-21 22:10:32 -0800

    surrounded showcard with a tag and renamed show as show-card, to bring into alignment with css

--- src/ShowCard.js
+++ src/ShowCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const ShowCard = (props) => (
-	<div className='show' key={props.show.imdbID}>
-		<img className='show-card-img' src={`./public/img/posters/${props.show.poster}`} />
-		<div className='show-card-text' key={props.show.imdbID}>
-			<h3 className='show-card-title'>{props.show.title}</h3>
-			<h4 className='show-card-year'>{props.show.year}</h4>
-			<p className='show-card-description'>{props.show.description}</p>
+	<a href="">
+		<div className='show-card' key={props.show.imdbID}>
+			<img className='show-card-img' src={`./public/img/posters/${props.show.poster}`} />
+			<div className='show-card-text' key={props.show.imdbID}>
+				<h3 className='show-card-title'>{props.show.title}</h3>
+				<h4 className='show-card-year'>{props.show.year}</h4>
+				<p className='show-card-description'>{props.show.description}</p>
+			</div>
 		</div>
-	</div>
+	</a>
 );
 
 module.exports = ShowCard;
